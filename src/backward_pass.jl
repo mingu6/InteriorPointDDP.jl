baf229f11360cae6e070bbdb141ccd283132fcf9
Pranav <u7286976@anu.edu.au>
2023-09-12 12:45:42 +1000

    prelim backward_pass code (unfinished)

--- src/backward_pass.jl
+++ src/backward_pass.jl
@@ -1,21 +1,66 @@
-function backward_pass!(policy::PolicyData, problem::ProblemData; 
-    mode=:nominal)
+function backward_pass!(policy::PolicyData, 
+    problem::ProblemData; 
+    mode=:nominal, 
+    horizon::Int128, 
+    constraint_data::ConstraintData)
 
-    # horizon 
-    H = length(problem.states)
+    # Horizon 
+    H = horizon
 
-    # gradients 
+    # Dimensions 
+    dim_x = length(problem.states)
+    dim_u = length(problem.actions)
+    dim_c = length(constraint_data.constraints)
+
+    # Errors
+    c_err::Float64 = 0
+    mu_err::Float64 = 0
+    Qu_err::Float64 = 0
+
+    # Current trajectory
+    x = problem.states
+    u = problem.actions
+
+    # Dual variables 
+    # TODO: Interface with dual variables properly
+    s = problem.dual.eq
+    y = problem.dual.ineq
+    
+    # Jacobians of system dynamics
     fx = problem.model.jacobian_state
     fu = problem.model.jacobian_action
-    gx = problem.objective.gradient_state
-    gu = problem.objective.gradient_action
+    # Hessians of system dynamics 
+    fxx = problem.model.hessian_state_state
+    fxu = problem.model.hessian_state_action
+    fuu = problem.model.hessian_action_action
+    # Jacobian constraints 
+    Qsx = constraint_data.jacobian_state
+    Qsu = constraint_data.jacobian_action
 
-    # Hessians
-    gxx = problem.objective.hessian_state_state
-    guu = problem.objective.hessian_action_action
-    gux = problem.objective.hessian_action_state
+    # Cost 
+    q = problem.objective.costs
+    # Cost gradients
+    qx = problem.objective.gradient_state
+    qu = problem.objective.gradient_action
+    # Cost hessians
+    qxx = problem.objective.hessian_state_state
+    quu = problem.objective.hessian_action_action
+    qux = problem.objective.hessian_action_state
+
+    # Value function approximation
+    V = policy.value
+    Vx = policy.value.gradient
+    Vxx = policy.value.hessian
+
+    # Action-Value function approximation
+    Qx = policy.action_value.gradient_state
+    Qu = policy.action_value.gradient_action
+    Qxx = policy.action_value.hessian_state_state
+    Quu = policy.action_value.hessian_action_action
+    Qux = policy.action_value.hessian_action_state
 
     # policy
+    # TODO: Update policy to include dual variables
     if mode == :nominal
         K = policy.K
         k = policy.k
@@ -24,44 +69,40 @@ function backward_pass!(policy::PolicyData, problem::ProblemData;
         k = policy.k_cand
     end
 
-    # value function approximation
-    P = policy.value.hessian
-    p = policy.value.gradient
-
-    # state-action value function approximation
-    Qx = policy.action_value.gradient_state
-    Qu = policy.action_value.gradient_action
-    Qxx = policy.action_value.hessian_state_state
-    Quu = policy.action_value.hessian_action_action
-    Qux = policy.action_value.hessian_action_state
-
     # terminal value function
-    P[H] .= gxx[H]
-    p[H] .= gx[H]
+    # TODO: Update terminal value function - is this correct?
+    # TODO: Implement without possibility of terminal constraints - this is an extension 
+    Vxx[H] .= qxx[H]
+    Vx[H] .= qx[H]
 
     for t = H-1:-1:1
-        # Qx[t] .= gx[t] + fx[t]' * p[t+1]
-        mul!(Qx[t], transpose(fx[t]), p[t+1])
-        Qx[t] .+= gx[t]
+        # TODO: Question - Why do we multiply the jacobian state by t+1th timestep of the value function?
+
+        # Qx[t] .= qx[t] + (Qsx[t]' * s)+ (fx[t]' * Vx[t+1])
+        # TODO: Need to pre-allocate the matrices here in the policy struct and then add
+        # TODO: Optimise tensordot and tensor-vector product
+        mul!(t1, transpose(fx[t]), Vx[t+1])
+        mul!(t2, transpose(Qsx[t]), s)
+        Qx[t] .+= qx[t] + t1 + t2
 
         # Qu[t] .= gu[t] + fu[t]' * p[t+1]
-        mul!(Qu[t], transpose(fu[t]), p[t+1])
-        Qu[t] .+= gu[t]
+        mul!(Qu[t], transpose(fu[t]), Vx[t+1])
+        Qu[t] .+= qu[t]
 
         # Qxx[t] .= gxx[t] + fx[t]' * P[t+1] * fx[t]
-        mul!(policy.xx̂_tmp[t], transpose(fx[t]), P[t+1])
+        mul!(policy.xx̂_tmp[t], transpose(fx[t]), Vxx[t+1])
         mul!(Qxx[t], policy.xx̂_tmp[t], fx[t])
-        Qxx[t] .+= gxx[t]
+        Qxx[t] .+= qxx[t]
 
         # Quu[t] .= guu[t] + fu[t]' * P[t+1] * fu[t]
-        mul!(policy.ux̂_tmp[t], transpose(fu[t]), P[t+1])
+        mul!(policy.ux̂_tmp[t], transpose(fu[t]), Vxx[t+1])
         mul!(Quu[t], policy.ux̂_tmp[t], fu[t])
-        Quu[t] .+= guu[t]
+        Quu[t] .+= quu[t]
 
         # Qux[t] .= gux[t] + fu[t]' * P[t+1] * fx[t]
-        mul!(policy.ux̂_tmp[t], transpose(fu[t]), P[t+1])
+        mul!(policy.ux̂_tmp[t], transpose(fu[t]), Vxx[t+1])
         mul!(Qux[t], policy.ux̂_tmp[t], fx[t])
-        Qux[t] .+= gux[t]
+        Qux[t] .+= qux[t]
 
         # K[t] .= -1.0 * Quu[t] \ Qux[t]
         # k[t] .= -1.0 * Quu[t] \ Qu[t]
@@ -78,14 +119,14 @@ function backward_pass!(policy::PolicyData, problem::ProblemData;
         # p[t] .=  Qx[t] + K[t]' * Quu[t] * k[t] + K[t]' * Qu[t] + Qux[t]' * k[t]
 		mul!(policy.ux_tmp[t], Quu[t], K[t])
 
-		mul!(P[t], transpose(K[t]), policy.ux_tmp[t])
-		mul!(P[t], transpose(K[t]), Qux[t], 1.0, 1.0)
-		mul!(P[t], transpose(Qux[t]), K[t], 1.0, 1.0)
-		P[t] .+= Qxx[t]
+		mul!(Vxx[t], transpose(K[t]), policy.ux_tmp[t])
+		mul!(Vxx[t], transpose(K[t]), Qux[t], 1.0, 1.0)
+		mul!(Vxx[t], transpose(Qux[t]), K[t], 1.0, 1.0)
+		Vxx[t] .+= Qxx[t]
 
-		mul!(p[t], transpose(policy.ux_tmp[t]), k[t])
-		mul!(p[t], transpose(K[t]), Qu[t], 1.0, 1.0)
-		mul!(p[t], transpose(Qux[t]), k[t], 1.0, 1.0)
-		p[t] .+= Qx[t]
+		mul!(Vx[t], transpose(policy.ux_tmp[t]), k[t])
+		mul!(Vx[t], transpose(K[t]), Qu[t], 1.0, 1.0)
+		mul!(Vx[t], transpose(Qux[t]), k[t], 1.0, 1.0)
+		Vx[t] .+= Qx[t]
     end
 end
--- src/data/model.jl
+++ src/data/model.jl
@@ -2,17 +2,21 @@
     Model Data
 """
 
-struct ModelData{T,X,U,W}
+struct ModelData{T,X,U,W,XX,XU,UU}
     dynamics::Vector{Dynamics{T}}
     jacobian_state::Vector{X}
     jacobian_action::Vector{U}
-	jacobian_parameter::Vector{W}
+	jacobian_parameter::Vector{W} # TODO: Remove
+    hessian_state_state::Vector{XX}
+    hessian_state_action::Vector{XU}
+    hessian_action_action::Vector{UU}
 end
 
 function model_data(dynamics::Vector{Dynamics{T}}) where T
 	jacobian_state = [zeros(d.num_next_state, d.num_state) for d in dynamics]
     jacobian_action = [zeros(d.num_next_state, d.num_action) for d in dynamics]
 	jacobian_parameter = [zeros(d.num_next_state, d.num_parameter) for d in dynamics]
+    # TODO: Initialse hessians 
     ModelData(dynamics, jacobian_state, jacobian_action, jacobian_parameter)
 end
 
