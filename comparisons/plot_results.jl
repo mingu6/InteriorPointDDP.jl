5628e8700b1075b3d92532e9cb101c08ed50e002
Ming Xu <mxu925@gmail.com>
2024-11-19 11:55:47 +0200

    modified summary results code to include wall and solver time

--- comparisons/plot_results.jl
+++ comparisons/plot_results.jl
@@ -2,14 +2,15 @@ using Printf
 using Statistics
 
 function read_results(fname::String)
-    regex_results = r"(\d+)(\s+)(\d+)(\s+)(\w+)(\s+)(\d+.\d+e[+-]\d+)(\s+)(\d+.\d+e[+-]\d+)(\s+)(\d+.\d+)  "
+    regex_results = r"(\d+)\s+(\d+)\s+(\w+)\s+(\d+.\d+e[+-]\d+)\s+(\d+.\d+e[+-]\d+)\s+(\d+.\d+)\s+(\d+.\d+)\s+(\d+.\d+)  "
     
     seeds = Int64[]
     iters = Int64[]
     statuss = Bool[]
     objs = Float64[]
     constrs = Float64[]
-    timings = Float64[]
+    walls = Float64[]
+    solvers = Float64[]
     
     open(fname, "r") do io
         lines = readlines(io)
@@ -17,126 +18,74 @@ function read_results(fname::String)
         for line in lines
             res = match(regex_results, line)
             if !isnothing(res)
-                seed, iter, status, obj, constr, timing = parse_results(res.captures)
+                seed, iter, status, obj, constr, wall, solver = parse_results(res.captures)
                 push!(seeds, seed)
                 push!(iters, iter)
                 push!(statuss, status)
                 push!(objs, obj)
                 push!(constrs, constr)
-                push!(timings, timing)
+                push!(walls, wall)
+                push!(solvers, solver)
             end
         end
     end
     
-    return seeds, iters, statuss, objs, constrs, timings
+    return seeds, iters, statuss, objs, constrs, walls, solvers
 end
 
 function parse_results(res)
     seed = parse(Int64, res[1])
-    iters = parse(Int64, res[3])
-    status = parse(Bool, res[5])
-    obj = parse(Float64, res[7])
-    constr = parse(Float64, res[9])
-    timing = parse(Float64, res[11])
-    return seed, iters, status, obj, constr, timing
+    iters = parse(Int64, res[2])
+    status = parse(Bool, res[3])
+    obj = parse(Float64, res[4])
+    constr = parse(Float64, res[5])
+    wall = parse(Float64, res[6])
+    solver = parse(Float64, res[7])
+    return seed, iters, status, obj, constr, wall, solver
+end
+
+function print_summary_results(io, fname, alg_name)
+    @printf(io, "%s \n", alg_name)
+    @printf(io, "\n")
+    _, iters, statuss, objs, constrs, wall, solver = read_results(fname)
+    @printf(io, "Iterations:  %s  %s  %s  %s  %s  \n", minimum(iters), quantile(iters, 0.25), median(iters), quantile(iters, 0.75), maximum(iters))
+    @printf(io, "Objective:  %.8e  %.8e  %.8e  %.8e  %.8e  \n", minimum(constrs), quantile(objs, 0.25), median(objs), quantile(objs, 0.75), maximum(constrs))
+    @printf(io, "Constraint Violation:  %.8e  %.8e  %.8e  %.8e  %.8e  \n", minimum(constrs), quantile(constrs, 0.25), median(constrs), quantile(constrs, 0.75), maximum(constrs))
+    @printf(io, "Wall (ms):  %5.1f  %5.1f  %5.1f  %5.1f  %5.1f \n", minimum(wall), quantile(wall, 0.25), median(wall), quantile(wall, 0.75), maximum(wall))
+    @printf(io, "Solver (ms):  %5.1f  %5.1f  %5.1f  %5.1f  %5.1f  \n", minimum(solver) ,quantile(solver, 0.25), median(solver), quantile(solver, 0.75), maximum(solver))
+    @printf(io, "Num. Failed:  %s, Num. Success:  %s", length(statuss) - sum(statuss), sum(statuss))
+    @printf(io, "\n")
 end
 
 fnames_in = ["../examples/results/concar.txt", "ipopt/results/concar.txt", "ALiLQR/results/concar.txt"]
-names = ["IPDDP2", "IPOPT", "AL-iLQR"]
+names = ["IPDDP2", "IPDDP2 (QN)", "IPOPT", "AL-iLQR"]
 
 open("summary_results_concar.txt", "w") do io
     for (fname, name) in zip(fnames_in, names)
-        @printf(io, "%s \n", name)
-        @printf(io, "\n")
-        seeds, iters, statuss, objs, constrs, timings = read_results(fname)
-        @printf(io, "Iterations:  %s  %s  %s \n", quantile(iters, 0.25), median(iters), quantile(iters, 0.75))
-        @printf(io, "Objective:  %.8e  %.8e  %.8e \n", quantile(objs, 0.25), median(objs), quantile(objs, 0.75))
-        @printf(io, "Constraint Violation:  %.8e  %.8e  %.8e \n", quantile(constrs, 0.25), median(constrs), quantile(constrs, 0.75))
-        @printf(io, "Wall Clock (s):  %.5f  %.5f  %.5f \n", quantile(timings, 0.25), median(timings), quantile(timings, 0.75))
-        @printf(io, "\n")
+        print_summary_results(io, fname, name)
     end
 end
 
-fnames_in = ["../examples/results/cartpole_implicit.txt", "ipopt/results/cartpole.txt", "ALiLQR/results/cartpole_implicit.txt"]
-names = ["IPDDP2", "IPOPT", "AL-iLQR"]
+fnames_in = ["../examples/results/cartpole_implicit.txt", "../examples/results/cartpole_implicit_QN.txt", "ipopt/results/cartpole.txt", "ALiLQR/results/cartpole_implicit.txt"]
 
 open("summary_results_cartpole_implicit.txt", "w") do io
     for (fname, name) in zip(fnames_in, names)
-        @printf(io, "%s \n", name)
-        @printf(io, "\n")
-        seeds, iters, statuss, objs, constrs, timings = read_results(fname)
-        @printf(io, "Iterations:  %s  %s  %s \n", quantile(iters, 0.25), median(iters), quantile(iters, 0.75))
-        @printf(io, "Objective:  %.8e  %.8e  %.8e \n", quantile(objs, 0.25), median(objs), quantile(objs, 0.75))
-        @printf(io, "Constraint Violation:  %.8e  %.8e  %.8e \n", quantile(constrs, 0.25), median(constrs), quantile(constrs, 0.75))
-        @printf(io, "Wall Clock (s):  %.5f  %.5f  %.5f \n", quantile(timings, 0.25), median(timings), quantile(timings, 0.75))
-        @printf(io, "\n")
+        print_summary_results(io, fname, name)
     end
 end
 
-fnames_in = ["../examples/results/blockmove.txt", "ipopt/results/blockmove.txt", "ALiLQR/results/blockmove.txt"]
-names = ["IPDDP2", "IPOPT", "AL-iLQR"]
+fnames_in = ["../examples/results/blockmove.txt", "../examples/results/blockmove_QN.txt", "ipopt/results/blockmove.txt", "ALiLQR/results/blockmove.txt"]
 
 open("summary_results_blockmove.txt", "w") do io
     for (fname, name) in zip(fnames_in, names)
-        @printf(io, "%s \n", name)
-        @printf(io, "\n")
-        seeds, iters, statuss, objs, constrs, timings = read_results(fname)
-        @printf(io, "Iterations:  %s  %s  %s \n", quantile(iters, 0.25), median(iters), quantile(iters, 0.75))
-        @printf(io, "Objective:  %.8e  %.8e  %.8e \n", quantile(objs, 0.25), median(objs), quantile(objs, 0.75))
-        @printf(io, "Constraint Violation:  %.8e  %.8e  %.8e \n", quantile(constrs, 0.25), median(constrs), quantile(constrs, 0.75))
-        @printf(io, "Wall Clock (s):  %.5f  %.5f  %.5f \n", quantile(timings, 0.25), median(timings), quantile(timings, 0.75))
-        @printf(io, "\n")
+        print_summary_results(io, fname, name)
     end
 end
 
-fnames_in = ["../examples/results/acrobot_contact.txt", "ipopt/results/acrobot_contact.txt", "ALiLQR/results/acrobot_contact.txt"]
-names = ["IPDDP2", "IPOPT", "AL-iLQR"]
+fnames_in = ["../examples/results/acrobot_contact.txt", "../examples/results/acrobot_contact_QN.txt", "ipopt/results/acrobot_contact.txt", "ALiLQR/results/acrobot_contact.txt"]
 
 open("summary_results_acrobot_contact.txt", "w") do io
     for (fname, name) in zip(fnames_in, names)
-        @printf(io, "%s \n", name)
-        @printf(io, "\n")
-        seeds, iters, statuss, objs, constrs, timings = read_results(fname)
-        @printf(io, "Iterations:  %s  %s  %s \n", quantile(iters, 0.25), median(iters), quantile(iters, 0.75))
-        @printf(io, "Objective:  %.8e  %.8e  %.8e \n", quantile(objs, 0.25), median(objs), quantile(objs, 0.75))
-        @printf(io, "Constraint Violation:  %.8e  %.8e  %.8e \n", quantile(constrs, 0.25), median(constrs), quantile(constrs, 0.75))
-        @printf(io, "Wall Clock (s):  %.5f  %.5f  %.5f \n", quantile(timings, 0.25), median(timings), quantile(timings, 0.75))
-        @printf(io, "\n")
+        print_summary_results(io, fname, name)
     end
 end
-
-
-# function plot_iteration_count(fnames_in::Vector{String}, names::Vector{String})
-#     plot()
-#     for (fname, name) in zip(fnames_in, names)
-#         seeds, iters, statuss, objs, constrs, timings = read_results(fname)
-#         histogram!(iters, linecolor=1, xtickfontsize=14, ytickfontsize=14, legendfontsize=14, bins=range(150, 400, length=10), label=name)
-#     end
-#     savefig("iteration_count.png")
-# end
-
-# function plot_objective(fnames_in::Vector{String}, names::Vector{String})
-#     plot()
-#     for (fname, name) in zip(fnames_in, names)
-#         seeds, iters, statuss, objs, constrs, timings = read_results(fname)
-#         histogram!(objs, linecolor=1, xtickfontsize=14, ytickfontsize=14, legendfontsize=14, bins=15, label=name)
-#     end
-#     savefig("objective.png")
-# end
-
-# function plot_violation(fnames_in::Vector{String}, names::Vector{String})
-#     plot()
-#     for (fname, name) in zip(fnames_in, names)
-#         seeds, iters, statuss, objs, constrs, timings = read_results(fname)
-#         clamp!(constrs, 1e-16, Inf)
-#         histogram!(constrs, linecolor=1, xtickfontsize=14, ytickfontsize=14, legendfontsize=14, bins=15, label=name)
-#     end
-#     savefig("violation.png")
-# end
-
-# # plot_iteration_count(["../examples/results/concar.txt", "ipopt/results/concar.txt", "ALiLQR/results/concar.txt"])
-# plot_iteration_count(["../examples/results/concar.txt", "ALiLQR/results/concar.txt"], ["IPDDP2", "AL-iLQR"])
-# plot_objective(["../examples/results/concar.txt", "ALiLQR/results/concar.txt"], ["IPDDP2", "AL-iLQR"])
-# plot_violation(["../examples/results/concar.txt", "ALiLQR/results/concar.txt"], ["IPDDP2", "AL-iLQR"])
-
-# write_results(["../examples/results/concar.txt", "ALiLQR/results/concar.txt"], ["IPDDP2", "AL-iLQR"])
