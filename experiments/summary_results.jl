f054ab90dedff9488f701fe40b7d9d1874df7a96
mingu6 <mxu925@gmail.com>
2025-05-01 02:52:10 +1000

    updated plotting code

--- experiments/summary_results.jl
+++ experiments/summary_results.jl
@@ -3,7 +3,7 @@ using Statistics
 
 function read_results(fname::String)
     regex_results = r"\s*(\d+)\s+(\d+)\s+(\w+)\s+(\d+.\d+e[+-]\d+)\s+(\d+.\d+e[+-]\d+)\s+(\d+.\d+)\s+(\d+.\d+)"
-    regex_no_bm = r"\s*(\d+)\s+(\d+)\s+(\w+)\s+(\d+.\d+e[+-]\d+)\s+(\d+.\d+e[+-]\d+)"
+    regex_no_bm = r"\s*(\d+)\s+(\d+)\s+(\w+)\s+-?(\d+.\d+e?[+-]?\d+)?\s+(\d+.\d+e?[+-]?\d+)"
     
     seeds = Int64[]
     iters = Int64[]
@@ -65,65 +65,48 @@ end
 
 using StatsPlots
 
-# function plot_results(problemclass)
-#     fnames_in = ["ipddp2/results/$problemclass.txt", "ipopt/results/$problemclass.txt", "ALiLQR/results/$problemclass.txt"]
-#     names = ["IPDDP2", "IPOPT", "AL-iLQR"]
-#     metric = []
-#     method = []
-#     vals = []
-#     for (fname, name) in zip(fnames_in, names)
-#         _, iters, statuss, objs, constrs, wall, solver = read_results(fname)
-#         vals = [vals; objs; constrs; iters; wall]
-#         metric = [metric; ["Obj." for _ in 1:length(objs)]; ["Constr." for _ in 1:length(constrs)]; ["Iters." for _ in 1:length(iters)]; ["Time" for _ in 1:length(wall)]]
-#         method = [method; [name for _ in 1:length([objs; constrs; iters; wall])]]
-#     end
-#     groupedboxplot(metric, vals, group=method, legend=:bottom, bar_width=0.7)
-#     savefig("$problemclass.pdf")
-# end
-
-# function plot_results(problemclass)
-#     fnames_in = ["ipddp2/results/$problemclass.txt", "ipopt/results/$problemclass.txt", "ALiLQR/results/$problemclass.txt"]
-#     names = ["IPDDP2", "IPOPT", "AL-iLQR"]
-
-#     _, iters_ipd, _, objs_ipd, constrs_ipd, wall_ipd, _ = read_results("ipddp2/results/$problemclass.txt")
-#     _, iters_ipo, _, objs_ipo, constrs_ipo, wall_ipo, _ = read_results("ipopt/results/$problemclass.txt")
-#     _, iters_al, _, objs_al, constrs_al, wall_al, _ = read_results("ALiLQR/results/$problemclass.txt")
-
-#     boxplot(["IPDDP2" "IPOPT" "AL-iLQR"], [objs_ipd objs_ipo objs_al], title="Objective Function",
-#         yaxis=:log10, legend=false, size=(240, 400))
-#     savefig("objective_$problemclass.pdf")
-
-#     boxplot(["IPDDP2" "IPOPT" "AL-iLQR"], [constrs_ipd constrs_ipo constrs_al], title="Max Violation", yaxis=:log10, legend=false, size=(240, 400))
-#     savefig("constr_$problemclass.pdf")
-
-#     boxplot(["IPDDP2" "IPOPT" "AL-iLQR"], [iters_ipd iters_ipo iters_al], title="Iterations", yaxis=:log10, legend=false, size=(240, 400))
-#     savefig("iterations_$problemclass.pdf")
-
-#     boxplot(["IPDDP2" "IPOPT" "AL-iLQR"], [wall_ipd wall_ipo wall_al], title="Total time (ms)", yaxis=:log10, legend=false, size=(240, 400))
-#     savefig("time_$problemclass.pdf")
-# end
-
 function plot_results(problemclass)
-    fnames_in = ["ipddp2/results/$problemclass.txt", "ipopt/results/$problemclass.txt"]
-    names = ["IPDDP2", "IPOPT", "AL-iLQR"]
+    fnames_in = ["ipddp2/results/$problemclass.txt", "ipopt/results/$problemclass.txt", "proxddp/results/$problemclass.txt"]
+    names = ["IPDDP2", "IPOPT", "ProxDDP"]
+
+    if problemclass == "concar"
+        ylims_time = (0, 1450)
+        yticks_time = [0, 250, 500, 750, 1000, 1200]
+        yticks_viol = [1e-16, 1e-8, 1e-4, 1e-2, 1]
+        ylim_obs = (0.8, 2000)
+        yticks_obs = [1, 1e1, 1e2, 1e3]
+    elseif problemclass == "cartpole_friction"
+        ylims_time = (50, 6000)
+        yticks_time = [0, 1500, 3000, 4500, 6000]
+        yticks_viol = [1e-16, 1e-8, 1e-4, 1e-2, 1]
+        ylim_obs = (1e-3, 1e3)
+        yticks_obs = [1e-1, 1e1, 1e3]
+    end
 
     _, iters_ipd, _, objs_ipd, constrs_ipd, wall_ipd, _ = read_results("ipddp2/results/$problemclass.txt")
     _, iters_ipo, _, objs_ipo, constrs_ipo, wall_ipo, _ = read_results("ipopt/results/$problemclass.txt")
+    _, iters_ipob, _, objs_ipob, constrs_ipob, wall_ipob, _ = read_results("ipopt/results/bfgs_$problemclass.txt")
+    _, iters_al, _, objs_al, constrs_al, _, _ = read_results("proxddp/results/$problemclass.txt")
 
-    boxplot(["IPDDP2" "IPOPT"], [objs_ipd objs_ipo], title="Objective Function",
-        yaxis=:log10, legend=false, size=(200, 300))
-    savefig("objective_$problemclass.pdf")
+    boxplot(["IPDDP2" "IPOPT" "IPOPT (B)" "ProxDDP"], [objs_ipd objs_ipo objs_ipob objs_al], title="Objective Function",
+                yaxis=:log10, legend=false, size=(300, 300))
+    savefig("plots/$problemclass/objective.pdf")
 
-    boxplot(["IPDDP2" "IPOPT"], [constrs_ipd constrs_ipo], title="Max Violation", yaxis=:log10, legend=false, size=(200, 300))
-    savefig("constr_$problemclass.pdf")
+    boxplot(["IPDDP2" "IPOPT" "IPOPT (B)" "ProxDDP"], [constrs_ipd constrs_ipo constrs_ipob constrs_al], title="Max Violation",
+                yaxis=:log10, legend=false, size=(300, 300))
+    savefig("plots/$problemclass/constr.pdf")
 
-    boxplot(["IPDDP2" "IPOPT"], [iters_ipd iters_ipo], title="Iterations", legend=false, size=(200, 300))
-    savefig("iterations_$problemclass.pdf")
+    boxplot(["IPDDP2" "IPOPT" "IPOPT (B)" "ProxDDP"], [iters_ipd iters_ipo iters_ipob iters_al], title="Iterations",
+                yaxis=:log10, legend=false, size=(300, 300))
+    savefig("plots/$problemclass/iterations.pdf")
 
-    boxplot(["IPDDP2" "IPOPT"], [wall_ipd wall_ipo], title="Total time (ms)", legend=false, size=(200, 300))
-    savefig("time_$problemclass.pdf")
+    boxplot(["IPDDP2" "IPOPT" "IPOPT (B)"], [wall_ipd wall_ipo wall_ipob], legend=false, size=(300, 300),
+                )
+    savefig("plots/$problemclass/time.pdf")
 end
 
-# plot_results("cartpole_inverse")
-# plot_results("concar")
-plot_results("pushing")
+plot_results("cartpole_friction")
+plot_results("cartpole_friction_quad")
+plot_results("concar")
+plot_results("acrobot_contact")
+plot_results("pushing_1_obs")
